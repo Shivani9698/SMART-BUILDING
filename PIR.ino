47e8f31aab56a72fb588dbd7502b3623542a53dd
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-01-19 10:42:08 +0530

    Update PIR.ino

--- PIR.ino
+++ PIR.ino
@@ -1,9 +1,8 @@
 // Template ID, Device Name, and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab or Template settings
-#define BLYNK_TEMPLATE_ID "TMPL3XUDIMBEO"
-#define BLYNK_TEMPLATE_NAME "POT AND LED"
-#define BLYNK_AUTH_TOKEN "OiGBv7x6OfDuRGdiLiz840vmf6y70wP3"
-
+#define BLYNK_TEMPLATE_ID "TMPL36a_ZUDO6"
+#define BLYNK_TEMPLATE_NAME "HOME AUTOMATION"
+#define BLYNK_AUTH_TOKEN "eNjIjpM38fJQSqFWpYc-7c08UWN_OJy_"
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 #define timeSeconds 10
