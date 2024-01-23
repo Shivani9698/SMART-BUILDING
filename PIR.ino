c7f546f0064dc9a11bf494008eb45abb77521297
Shivani9698 <119753029+Shivani9698@users.noreply.github.com>
2024-01-24 00:34:17 +0530

    Update PIR.ino

--- PIR.ino
+++ PIR.ino
@@ -1,11 +1,11 @@
 // Template ID, Device Name, and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab or Template settings
-#define BLYNK_TEMPLATE_ID "TMPL36a_ZUDO6"
-#define BLYNK_TEMPLATE_NAME "HOME AUTOMATION"
-#define BLYNK_AUTH_TOKEN "eNjIjpM38fJQSqFWpYc-7c08UWN_OJy_"
+#define BLYNK_TEMPLATE_ID "TMPL3XUDIMBEO"
+#define BLYNK_TEMPLATE_NAME "POT AND LED"
+#define BLYNK_AUTH_TOKEN "sfYj2irrsPy_DoBXn3UPrKQ40ZynWsVC"
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
-#define timeSeconds 10
+
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
@@ -14,56 +14,45 @@ char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials
 // Set password to "" for open networks
-char ssid[] = "FTTHBSNL";
-char pass[] = "29915693";
+char ssid[] = "AirFiber";
+char pass[] = "Hnasetty09";
 
 BlynkTimer timer;
-const int led = D2; // Change this to the appropriate GPIO pin for your board
-const int motionSensor = D6; // Change this to the appropriate GPIO pin for your board
+int Status = 2;  // Digital pin D6
+
+int sensor = 13;  // Digital pin D7
 
 // Timer: Auxiliary variables
-unsigned long now = millis();
-unsigned long lastTrigger = 0;
-boolean startTimer = false;
-boolean motion = false;
-
-// Checks if motion was detected, sets LED HIGH, and starts a timer
-void detectsMovement() {
-  digitalWrite(led, HIGH);
-  startTimer = true;
-  lastTrigger = millis();
-}
 
 void setup() {
-  pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);
-
-  // Set LED to LOW
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-  
-  // Debug console
   Serial.begin(115200);
+
+  pinMode(sensor, INPUT);   // declare sensor as input
+  pinMode(Status, OUTPUT);  // declare LED as output
 
   Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
   Blynk.run();
+  timer.run();
+  
+  delay(2000);
 
-  now = millis();
-  if ((digitalRead(led) == HIGH) && (motion == false)) {
-    Serial.println("MOTION DETECTED!!!");
-    motion = true;
-  }
-  // Turn off the LED after the number of seconds defined in the timeSeconds variable
-  if (startTimer && (now - lastTrigger > (timeSeconds * 1000))) {
-    Serial.println("Motion stopped...");
-    digitalWrite(led, LOW);
-    startTimer = false;
-    motion = false;
-  }
+ long state = digitalRead(sensor);
+    if(state == HIGH) {
+      digitalWrite (Status, 0);
+      Serial.println("Motion detected!");
+      delay(1000);
+      
+    }
+    else {
+      digitalWrite (Status, 1);
+      Serial.println("Motion absent!");
+      delay(1000);
+      
+      }
+  
 
-  Blynk.virtualWrite(V6, motion);
+  Blynk.virtualWrite(V6, state);
 }
